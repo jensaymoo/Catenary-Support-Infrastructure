e567e619dc2e799dac40d68d7fb815fe9870bf6f
pigonthewind <pigonthewind@pm.me>
2024-02-18 11:20:38 +0300

    добавил провайдера для протоколов

---
+++ Catenary Support Infrastructure/Providers/ProtocolProvider.cs
@@ -0,0 +1,47 @@
+﻿using CatenarySupport.Database;
+using CatenarySupport.Providers.Objects;
+using System.Linq.Expressions;
+
+namespace CatenarySupport.Providers
+{
+    internal class ProtocolProvider : IProvider<ProtocolObject>
+    {
+        private readonly IDatabase database;
+        public ProtocolProvider(IDatabase db)
+        {
+            database = db;
+        }
+
+        public void Insert(ProtocolObject model)
+        {
+            database.Insert(model);
+        }
+
+        public void Delete(ProtocolObject model)
+        {
+            database.Delete(model);
+        }
+        public void Delete(Expression<Func<ProtocolObject, bool>> predicate)
+        {
+            database.Delete(predicate);
+        }
+
+        public IEnumerable<ProtocolObject> Select()
+        {
+            
+            return database.Select<ProtocolObject>();
+        }
+
+        public IEnumerable<ProtocolObject> Select(Expression<Func<ProtocolObject, bool>> predicate)
+        {
+            return database.Select(predicate);
+        }
+
+        public void Update(ProtocolObject model)
+        {
+            database.Update(model);
+        }
+
+
+    }
+}
